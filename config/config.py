eb1106922d10297d315c5e5161c074fcb405de37
Potter Hsu <potterhsu0818@gmail.com>
2018-12-28 15:37:14 +0800

    Change default pooling mode to `align`

--- config/config.py
+++ config/config.py
@@ -12,7 +12,7 @@ class Config(object):
 
     ANCHOR_RATIOS: List[Tuple[int, int]] = [(1, 2), (1, 1), (2, 1)]
     ANCHOR_SIZES: List[int] = [128, 256, 512]
-    POOLING_MODE: ROIWrapper.Mode = ROIWrapper.Mode.POOLING
+    POOLING_MODE: ROIWrapper.Mode = ROIWrapper.Mode.ALIGN
 
     @classmethod
     def describe(cls):
