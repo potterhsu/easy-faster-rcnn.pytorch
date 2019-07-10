2c30c6d4ea57402c813294a499181b6ad710f858
potterhsu <potterhsu0818@gmail.com>
2019-07-10 17:23:49 +0800

    Fix #11

--- bbox.py
+++ bbox.py
@@ -43,8 +43,8 @@ class BBox(object):
         center_based_src_bboxes = BBox.to_center_base(src_bboxes)
         center_based_dst_bboxes = BBox.to_center_base(dst_bboxes)
         transformers = torch.stack([
-            (center_based_dst_bboxes[..., 0] - center_based_src_bboxes[..., 0]) / center_based_dst_bboxes[..., 2],
-            (center_based_dst_bboxes[..., 1] - center_based_src_bboxes[..., 1]) / center_based_dst_bboxes[..., 3],
+            (center_based_dst_bboxes[..., 0] - center_based_src_bboxes[..., 0]) / center_based_src_bboxes[..., 2],
+            (center_based_dst_bboxes[..., 1] - center_based_src_bboxes[..., 1]) / center_based_src_bboxes[..., 3],
             torch.log(center_based_dst_bboxes[..., 2] / center_based_src_bboxes[..., 2]),
             torch.log(center_based_dst_bboxes[..., 3] / center_based_src_bboxes[..., 3])
         ], dim=-1)
